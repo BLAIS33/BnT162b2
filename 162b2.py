2dd1d36e770ca550b4e011668caf7ade7841388f
mirai <60798189+mirai101@users.noreply.github.com>
2021-03-30 16:51:08 +0800

    Update 162b2.py

--- 162b2.py
+++ 162b2.py
@@ -4,13 +4,16 @@ import mmap
 
 codon_usage_table = python_codon_tables.get_codons_table(57486)
 species = 'h_sapiens'
-#TaxIDs: 10090,57486 (mus musculus) 9544 (macaca mulatta) 9606 (homo s>
+#TaxIDs: 10090,57486 (mus musculus) 9544 (macaca mulatta) 9606 (homo sapiens)
 
 def mmap_io(filename):
     with open(filename, mode="r", encoding="utf8") as file_obj:
-        with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS>
+        with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
             return mmap_obj.read().decode("UTF-8").split("\n")[:-1]
-                                                                       input = mmap_io("codon.txt")                                                                                                                 virus,vaccine = input[0],input[1]
+
+input = mmap_io("codons.txt")
+
+virus,vaccine = input[0],input[1]
 
 def compute_match(one, two):
     num_mismatches = 0
@@ -19,13 +22,15 @@ def compute_match(one, two):
             num_mismatches = num_mismatches + 1
     return (1 - float(num_mismatches) / float(len(one)))
 
-def optimize_virus():                                                      problem = DnaOptimizationProblem(
+def optimize_virus():
+    problem = DnaOptimizationProblem(
         sequence=virus,
         constraints=[
-            EnforceTranslation(genetic_table='Standard', start_codon='>            EnforceGCContent(mini=0.54, maxi=0.93, window=120),
+            EnforceTranslation(genetic_table='Standard', start_codon='ATG'),
+            EnforceGCContent(mini=0.54, maxi=0.93, window=120),
         ],
         objectives=[
-            CodonOptimize(method = "use_best_codon", codon_usage_table>
+            CodonOptimize(method = "use_best_codon", codon_usage_table=codon_usage_table),
         ]
     )
     problem.resolve_constraints()
